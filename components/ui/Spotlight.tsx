f77b53fec5836a8311d237e2b120f01bac378d1d
Kaio Jansen <54814187+Kaiojansen@users.noreply.github.com>
2024-09-04 02:57:46 -0300

    test

--- components/ui/Spotlight.tsx
+++ components/ui/Spotlight.tsx
@@ -3,14 +3,19 @@ import React from "react";
 
 type SpotlightProps = {
   className?: string;
-  fill?: string;
+  fillGreen?: string;
+  fillYellow?: string;
 };
 
-export const Spotlight = ({ className, fill }: SpotlightProps) => {
+export const Spotlight = ({
+  className,
+  fillGreen = "green",
+  fillYellow = "yellow",
+}: SpotlightProps) => {
   return (
     <svg
       className={cn(
-        "animate-spotlight pointer-events-none absolute z-[1]  h-[169%] w-[138%] lg:w-[84%] opacity-0",
+        "animate-spotlight pointer-events-none absolute z-[1] h-[169%] w-[138%] lg:w-[84%] opacity-0",
         className
       )}
       xmlns="http://www.w3.org/2000/svg"
@@ -18,15 +23,26 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
       fill="none"
     >
       <g filter="url(#filter)">
+        {/* Elipse Verde */}
         <ellipse
           cx="1924.71"
           cy="273.501"
           rx="1924.71"
           ry="273.501"
           transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
-          fill={fill || "white"}
+          fill={fillGreen}
           fillOpacity="0.21"
-        ></ellipse>
+        />
+        {/* Elipse Amarela */}
+        <ellipse
+          cx="1500"
+          cy="500"
+          rx="1924.71"
+          ry="273.501"
+          transform="matrix(0.822377 -0.568943 0.568943 0.822377 1155.88 1000.09)"
+          fill={fillYellow}
+          fillOpacity="0.21"
+        />
       </g>
       <defs>
         <filter
@@ -38,17 +54,17 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
-          <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feBlend
             mode="normal"
             in="SourceGraphic"
             in2="BackgroundImageFix"
             result="shape"
-          ></feBlend>
+          />
           <feGaussianBlur
             stdDeviation="151"
             result="effect1_foregroundBlur_1065_8"
-          ></feGaussianBlur>
+          />
         </filter>
       </defs>
     </svg>
